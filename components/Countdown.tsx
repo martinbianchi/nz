ba9365e3ac85160a4abd9da4dc7db6b10ec90902
Martin Bianchi <tinchola7@gmail.com>
2023-01-12 20:38:20 -0300

    fix date

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -1,7 +1,5 @@
 import FlipCountdown from "@rumess/react-flip-countdown";
 
 export const Countdown = () => {
-  return (
-    <FlipCountdown theme="dark" endAt="2/3/2023 15:00" hideYear hideMonth />
-  );
+  return <FlipCountdown theme="dark" endAt="3/2/2023 15:00" hideYear />;
 };
